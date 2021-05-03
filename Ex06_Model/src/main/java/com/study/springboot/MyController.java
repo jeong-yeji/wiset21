613f32aa19740dbfd10d1ab453ac392800571eed
jeong-yeji <jeongyeji.mail@gmail.com>
2021-05-03 19:45:06 +0900

    Web 기초 - Form

---
+++ Ex06_Model/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,47 @@
+package com.study.springboot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+
+	@RequestMapping("/")
+	public @ResponseBody String root() throws Exception {
+		return "Model & View";
+	}
+	
+	@RequestMapping("/test1")				// localhost:8081/test1
+	public String test1(Model model) {
+		// Model 객체를 이용해서, view로 data 전달
+		// 데이터만 설정 가능
+		model.addAttribute("name", "홍길동"); // name을 홍길동으로 지정해서 넘겨줌
+		
+		return "test1";						// 실제 호출될 /WEB-INF/views/test1.jsp
+	}
+	
+	@RequestMapping("/mv")
+	public ModelAndView test2(Model model) {
+		// 데이터와 뷰를 동시에 설정 가능
+		ModelAndView mv = new ModelAndView();
+		
+		List<String> list = new ArrayList<>();
+		
+		list.add("test1");
+		list.add("test2");
+		list.add("test3");
+		
+		mv.addObject("lists", list);			// jstl로 호출
+		mv.addObject("ObjectTest", "테스트입니다.");// jstl로 호출
+		mv.addObject("name", "홍길동");			// jstl로 호출
+		mv.setViewName("view/myView");			// 실제로 호출될 /WEB-INF/views/myView.jsp
+		
+		return mv;
+	}
+}
