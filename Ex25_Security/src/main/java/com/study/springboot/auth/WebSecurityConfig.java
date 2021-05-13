3bd0fcfe0389927e2a24f9294d91a0c1312a02aa
jeong-yeji <jeongyeji.mail@gmail.com>
2021-05-13 21:53:44 +0900

    Security

---
+++ Ex25_Security/src/main/java/com/study/springboot/auth/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.study.springboot.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/").permitAll()
+			.antMatchers("/css/**", "/js/**", "/img/**").permitAll()
+			.antMatchers("/guest/**").permitAll()
+			.antMatchers("/member/**").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/admin/**").hasRole("ADMIN")
+			.anyRequest().authenticated();
+		
+		http.formLogin().permitAll();
+		http.logout().permitAll();
+	}
+	
+	// 빠른 테스트를 위해 등록이 간단한 inMemory 방식의 인증 사용자 등록 => 테스트할때만 사용
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("user").password(passwordEncoder().encode("1234")).roles("USER")
+			.and()
+			.withUser("admin").password(passwordEncoder().encode("1234")).roles("ADMIN");
+		// ROLE_ADMIN에서 ROLE_은 자동으로 붙는다.
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
